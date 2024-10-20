6fa32241c3a858e19ac50ae3cd0e8b519f33a7aa
AlekSnake <alexunderas16@gmail.com>
2024-10-20 23:54:27 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+name = 'my_string'
+print('my_string'.upper())
+print('my_string'.lower())
+print(len('my_string'))
+print('myString')
+print('my_string'[-1])
+print('my_string' [0])
