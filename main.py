fd8156c3a9ec4036da738f8cd3ea7a61f621387b
AlekSnake <alexunderas16@gmail.com>
2024-10-26 22:26:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-name = 'my_string'
-print('my_string'.upper())
-print('my_string'.lower())
-print(len('my_string'))
-print('myString')
-print('my_string'[-1])
-print('my_string' [0])
+my_string = input('Введите текст: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
